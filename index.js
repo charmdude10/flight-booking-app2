e0beff67759f102a4e642b054ee525f8aee2c88c
charmdude10 <abioyemichael4@gmail.com>
2025-01-08 10:37:45 +0100

    my first backend commit message

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Prisma } from "@prisma/client";
+import cors from "cors";
+import express from "express";
+
+function index() {
+  return <div></div>;
+}
+
+export default index;
