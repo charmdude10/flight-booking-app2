d6b69d142edc3d1f9ad9418495bf1c45727c2b9a
charmdude10 <abioyemichael4@gmail.com>
2025-01-09 17:19:55 +0100

    my second commit message

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-import React from "react";
-import { Prisma } from "@prisma/client";
-import cors from "cors";
+// const express = require('express');
+// const cors = require('cors');
+// const authRoutes = require('./routes/authRoutes');
 import express from "express";
+import cors from "cors";
+import corsOptions from "./config/corsOptions.js";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authRoutes.js";
 
-function index() {
-  return <div></div>;
-}
+const app = express();
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//built-in middleware for json
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 export default index;
