74c9c7ec6563048ec3c8a4e24a9e62eaee04c829
charmdude10 <abioyemichael4@gmail.com>
2025-01-10 16:23:09 +0100

    payment.js created,index.js fixed,autControllers fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,6 +17,13 @@ export const register = async (req, res) => {
   } catch (error) {
     res.status(400).json("User already exists");
   }
+
+  ///////////////
+  if (!username || !password) {
+    return res.status(400).json({ message: "All fields are required." });
+  }
+  // Mock response for registration
+  res.status(201).json({ message: "User registered successfully." });
 };
 
 export const login = async (req, res) => {
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
+// Middleware to parse JSON
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,11 +19,10 @@ app.use(cookieParser());
 //built-in middleware for json
 app.use(express.json());
 
+// Connect auth routes
 app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-
