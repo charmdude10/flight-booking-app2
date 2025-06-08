9365a13361275462fb9f02cc5e80368e5b219476
charmdude10 <abioyemichael4@gmail.com>
2025-06-08 23:55:25 +0100

    added delete controller

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   createBooking,
+  deleteBooking,
   getAllBookings,
 } from "../controllers/bookingController.js";
 import { authMiddleware } from "../middlewares/authMiddlewares.js";
@@ -8,6 +9,12 @@ import { authMiddleware } from "../middlewares/authMiddlewares.js";
 const router = express.Router();
 
 router.post("/create-booking", authMiddleware, createBooking);
+//router for deletebooking
+router.delete("/delete-booking/:bookingId", authMiddleware, deleteBooking);
+
 router.get("/get-all-bookings", authMiddleware, getAllBookings);
 
 export default router;
+
+
+
